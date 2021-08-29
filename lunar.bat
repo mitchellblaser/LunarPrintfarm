ae136b4af930df45451c69bf6f81620d4fbdc231
Mitch Blaser <mitchell.1080media@gmail.com>
2021-08-30 00:47:03 +1000

    Added Script for WIN NT Clients.
    Same functionality as the batch version - will check if venv is initialized and if not will do so, then start the Flask server. Currently not using a WSGI server. Not super neccesary but might as well at some point?

--- lunar.bat
+++ lunar.bat
@@ -1,6 +1,14 @@
-python3 -m venv .lenv
-.lenv\Scripts\activate.bat
-pip install -r requirements.txt
+%~dp0 & "\.lenv"
 
+if exist %~dp0 & "\.lenv\" (
+    .lenv\Scripts\activate.bat
+) else (
+    echo "Setting up Lunar Virtual Environment for the first time."
+    python3 -m venv .lenv
+    .lenv\Scripts\activate.bat
+    pip install -r requirements.txt
+)
 
+echo "Starting Lunar."
 set FLASK_APP=lunar.py
+flask run
