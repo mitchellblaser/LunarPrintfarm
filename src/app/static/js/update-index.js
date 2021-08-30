ba8fa9b22fbf52f97027251893b8a457f9daf294
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-08-30 13:14:16 -0500

    Fixed info var
    Please let me know if this helps! Thank you. :)

--- src/app/static/js/update-index.js
+++ src/app/static/js/update-index.js
@@ -3,15 +3,18 @@ function initialUpdate() {
 }
 
 function doUpdate() {
+    let info;
+
     fetch("/api/info").then(r=> r.json().then(j=> info = j));
 
     for (var i=0; i<servers.length; i++) {
-
-        var status = info[servers[i]]['result']['status'];
-
-        document.getElementById(servers[i] + "_Percentage").innerText = "Progress: " + status['display_status']['progress'].toFixed(2) + "%";
-        document.getElementById(servers[i] + "_FileName").innerText = "Printing: " + status['print_stats']['filename'];
-
+        console.log(info)
+        if (info != undefined) {
+            var status = info[servers[i]]['result']['status'];
+    
+            document.getElementById(servers[i] + "_Percentage").innerText = "Progress: " + status['display_status']['progress'].toFixed(2) + "%";
+            document.getElementById(servers[i] + "_FileName").innerText = "Printing: " + status['print_stats']['filename'];
+        }
     }
 
 }
